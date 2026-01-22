66ed4b497a41c6b502df34e8576fe28476a0f0d5
ratola137 <lucasratola101@gmail.com>
2026-01-22 13:43:44 -0300

    feat/03
    clusterização e salvamento do arquivo em disco.

--- PYTHON/teste.py
+++ PYTHON/teste.py
@@ -8,7 +8,7 @@ file_path = 'PYTHON/dados_clientes.csv'
 data_set = pd.read_csv(file_path)
 
 #resumo estatístico dos dados
-print(data_set[['idade','renda_anual','pontuacao_gastos']].describe())
+data_set[['idade','renda_anual','pontuacao_gastos']].describe()
 
 #padronização dos dados - escala z-score dos dados
 padronizar = StandardScaler()
@@ -17,3 +17,14 @@ dados_padronizados = padronizar.fit_transform(data_set[['idade','renda_anual','p
 #demonstração a via de comparação dos dados originais e padronizados
 print(data_set[['idade','renda_anual','pontuacao_gastos']].head(10).to_string(index=False))
 print(dados_padronizados[:10])
+
+#define o número de clusters / segmentos
+k = 3
+#cria o modelo de KMeans
+kmeans = KMeans(n_clusters = k)
+#treina o modelo
+kmeans.fit(dados_padronizados)
+data_set['cluster'] = kmeans.labels_
+print(data_set.head(10).to_string(index=False))
+#salva o novo arquivo com os segmentos
+data_set.to_csv('PYTHON/dados_clientes_segmentados.csv', index=False)
