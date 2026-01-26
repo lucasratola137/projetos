e9376ac322f345db409a207f272fd0eb130e146a
Lucas <lucasratola101@gmail.com>
2026-01-26 17:30:14 -0300

    Update Query.sql

--- Query.sql
+++ Query.sql
@@ -3,18 +3,21 @@
 
 
 #agregação média apenas para validação
-SELECT a.cluster,avg(a.renda_anual) as media_renda ,avg(a.pontuacao_gasto) as media_gasto
-from projetocluster.clientes_segmentados a
-group by a.cluster;
+SELECT 
+  a.cluster,
+  avg(a.renda_anual) as media_renda,
+  avg(a.pontuacao_gasto) as media_gasto
+FROM projetocluster.clientes_segmentados a
+GROUP BY a.cluster;
 
 #query utilizada como fonte de dado para conexão com o Looker Studio
 SELECT 
 a.*, 
-case 
-when a.cluster = 2 then 'Ouro'
-when a.cluster = 1 then 'Prata'
-when a.cluster = 0 then 'Bronze'
-else 'not found'
-end as tipo_cliente
-from projetocluster.clientes_segmentados a
-order by a.idCliente asc;
+CASE
+WHEN a.cluster = 2 THEN 'Ouro'
+WHEN a.cluster = 1 THEN 'Prata'
+WHEN a.cluster = 0 THEN 'Bronze'
+ELSE 'not found'
+END AS tipo_cliente
+FROM projetocluster.clientes_segmentados a
+ORDER BY a.idCliente ASC;
